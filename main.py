d5c12adca3efd31abec650bbf3d05fd16f619b55
sigehiro <sigehiro.0728@gmail.com>
2024-10-01 16:37:10 -0400

    「１」の残高表示の実装と「２」の引き出し処理の実装

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+
+# balance
+balance = 1000
+
 def display_main_page():
     print('Entering 1 will display the balance')
     print('Entering 2 will allow making a withdraw')
@@ -7,12 +12,13 @@ def display_main_page():
     #loop:
     while True:
         choice = input('Enter the number 1 to 4: ')
+
         if choice == "1":
-            display_balance()
+            display_balance(balance)
         elif choice == "2":
-            make_withdraw()
+            make_withdraw(balance)
         elif choice =="3":
-            make_deposit()
+            make_deposit(balance)
         elif choice =="4":
             exit_program()
             break
@@ -22,15 +28,31 @@ def display_main_page():
 
 
 #残高表示
-def display_balance():
-    print('残高を表示')
+def display_balance(balance):
+    print(f'Your balance is ${balance:.2f}. \n')
+
 
 #引き出しの処理
-def make_withdraw():
-    print("引き出す金額は？")
+def make_withdraw(balance):
+
+    while True:
+        try:
+        #TODO ここでの入力で「０９」とか入れると＄991と読み込まれてしまう。
+            amount = float(input("What is amount of money to be withdrawn: "))
+            if amount > balance:
+                print("Balance is not enough\n")
+            else:
+                balance -= amount
+                print(f'Your balance is ${balance:.2f} \n')
+            
+                return balance
+        # except number
+        except ValueError:
+            print("Invalid input. Please enter a numeric value\n")
+
 
 #入金処理
-def make_deposit():
+def make_deposit(balance):
     print("入金を行います")
 
 # プログラム終了
